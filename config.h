c209c27f3467898ea9ea7337bea3983a0b2709dd
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-09-06 17:59:01 -0300

    Add files via upload

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 36;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
