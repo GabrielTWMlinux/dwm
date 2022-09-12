f9d3aa120069a433111562d723aa8e402bfeaf5d
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-09-12 03:05:30 -0300

    Add files via upload

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
-	{"  ^c#5c7e9a^ ", "~/.config/Scripts/Void-Updates",		3600,		10},
+	{"  ", "~/.config/Scripts/Void-Updates",		3600,		10},
 
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/weather",			3600,		0},
+	{"  ", "~/.config/Scripts/weather",			3600,		0},
 
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/volume-bar",		0,		5},
+	{"  ", "~/.config/Scripts/volume-bar",			0,		5},
 
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/ram-bar",			5,		0},
+	{"  ", "~/.config/Scripts/ram-bar",			5,		0},
 	
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/disk-bar",			3600,		0},
+	{"  ", "~/.config/Scripts/disk-bar",			3600,		0},
 
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/cpu-bar",			5,		0},
+	{"  ", "~/.config/Scripts/cpu-bar",			5,		0},
 
-	{" ^c#5c7e9a^ ", "~/.config/Scripts/clock-bar",		30,		0},
+	{"  ", "date '+%d %b %Y, %a %H:%M '",			30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
