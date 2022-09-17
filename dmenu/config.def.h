0c3804f8b9796cc3a0ad40216a8bb90c3f8bcd52
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-09-17 16:07:58 -0300

    Add files via upload

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -1,18 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+#include "/home/gabriel/.config/dwm/dmenu/themes/tokyonight.h"
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SymbolaRegular:size=9"
+	"SymbolaRegular:size=9" , "fontawesome:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#16161E" },
-	[SchemeSel] = { "#ffffff", "#345e81" },
-	[SchemeOut] = { "#000000", "#66887c" },
-};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
