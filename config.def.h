3dd8656af6ccbf84a2409727f5e4d4b4b7e53a76
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-12-20 17:54:11 -0300

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "/home/gabriel/.config/dwm/themes/green.h"
+#include "/home/gabriel/.config/dwm/themes/tokyo.h"
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
@@ -14,12 +14,12 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 19;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "SymbolaRegular:size=9:autohint=true:antialias=true" , "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true" };
+static const char *fonts[]          = { "SymbolaRegular:size=9:autohint=true:antialias=true" , "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "fontawesome:size=11";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray5, col_gray5  },
+	[SchemeSel]  = { col_gray4, col_gray5, col_gray3  },
 };
 
 /* tagging */
@@ -104,9 +104,10 @@ static Key keys[] = {
 	{ ControlMask, 	                XK_F9,     togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_space,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_space,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
