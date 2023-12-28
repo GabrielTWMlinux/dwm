9bd08ec8b14707b58f8a1d7a98945c2fe77cb535
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2023-12-28 02:12:33 -0300

    Add files via upload

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,19 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
-	{" 📥 ", "~/.config/Scripts/dwmblocks/Void-Updates",		3600,		10},
+	{"  ", "~/.config/Scripts/dwmblocks/Void-Updates",		3600,		10},
 
-	{" 🌤️ ", "~/.config/Scripts/dwmblocks/weather",			3600,		0},
+	{"  ", "~/.config/Scripts/dwmblocks/weather",		1800,		10},
 
-	{" 🎧 ", "~/.config/Scripts/dwmblocks/volume-bar",			1,		0},
+	{"  ", "~/.config/Scripts/dwmblocks/volume-bar",			1,		0},
 
-	{" 🗳️ ", "~/.config/Scripts/dwmblocks/ram-bar",			5,		0},
-	
-	{" ", "~/.config/Scripts/dwmblocks/disk-bar",			5,		0},
-
-	{" 🕑 ", "date '+%d %b %Y, %a %H:%M'",			30,		0},
+	{"  ", "date '+%d %b %Y, %a %H:%M'",			30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "   ";
 static unsigned int delimLen = 5;
